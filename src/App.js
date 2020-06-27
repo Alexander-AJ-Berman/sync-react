793a2c9e670823a746e4cc6c84afb11041d40540
AJ <alexander.berman97@gmail.com>
2020-06-27 11:55:19 -0400

    Setup Home layout, display grids (#1)
    Co-authored-by: Alexander-AJ-Berman <alexander_berman@alumni.brown.edu>

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import { Home } from "./pages/Home/Home";
+
+const AppContainer = styled.div`
+  height: 100%;
+  background-color: lightgray;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <Home />
+    </AppContainer>
+  );
+}
+
+export default App;
