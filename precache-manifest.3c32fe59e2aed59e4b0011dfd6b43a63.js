db930399a810ea931d2fe70ba8fb9edbf44aee21
Alexander-AJ-Berman <alexander_berman@alumni.brown.edu>
2020-06-27 12:04:35 -0400

    Updates

---
+++ precache-manifest.3c32fe59e2aed59e4b0011dfd6b43a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a04cc132a9b2d1ae9abd70be5702cb1",
+    "url": "/sync-react/index.html"
+  },
+  {
+    "revision": "e04fb6f610abcf720566",
+    "url": "/sync-react/static/css/main.2b199b94.chunk.css"
+  },
+  {
+    "revision": "c0e1bd4e57d411a6e0f8",
+    "url": "/sync-react/static/js/2.c29e3155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sync-react/static/js/2.c29e3155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04fb6f610abcf720566",
+    "url": "/sync-react/static/js/main.52fbc966.chunk.js"
+  },
+  {
+    "revision": "0b21c5055b226b41e6c6",
+    "url": "/sync-react/static/js/runtime-main.8cd93164.js"
+  }
+]);
